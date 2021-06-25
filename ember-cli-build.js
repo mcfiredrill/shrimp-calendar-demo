88d4bfd19edbe34216fe2dabc87c6c2684c60d1d
Tony Miller <mcfiredrill@gmail.com>
2021-06-25 12:35:04 +0900

    try disabling purgeCSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,7 +29,7 @@ module.exports = function (defaults) {
             },
           },
           require('tailwindcss')('./app/tailwind/config.js'),
-          ...(isProduction ? [purgeCSS] : []),
+          //...(isProduction ? [purgeCSS] : []),
         ],
       },
     },
